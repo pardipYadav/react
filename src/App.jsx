7ba8b3ff6c02d20fa0af50df2cfe4446356966a7
pardipYadav <213sunilyadav@gmail.com>
2025-05-15 05:22:26 +0530

    print hello world

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+const App = () => {
+  return (
+    <>
+      <div>
+        <Header />
+        <h1>Hello world</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <h1>This is header</h1>
+      </div>
+    </>
+  );
+};
+
+export default Header;
