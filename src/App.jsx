eda99cfc03cb4919f56238baa9c5eb1c9f7c1831
pardipYadav <213sunilyadav@gmail.com>
2025-05-20 04:50:17 +0530

    use state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import Login, { Setting } from "./UserLogin";
+import { useState } from "react";
 const App = () => {
+  const [name, setName] = useState("hello");
+  const [fruit, setfruite] = useState("banana");
+  const [count, setCount] = useState("1000");
+  const updataName = () => {
+    setName("world");
+    setfruite("mango");
+    setCount("500");
+  };
   return (
     <div>
-      <h1>Hello world</h1>
-      <Login />
-      <Setting />
+      <h1>State</h1>
+      <h1>The name is: {name} </h1>
+      <h1>Fruit: {fruit}</h1>
+      <h1>Count: {count}</h1>
+      <button onClick={updataName}>please update name</button>
     </div>
   );
 };
