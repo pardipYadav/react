b405d096a1d33a59a2dcecdefe5f7fcaf125fc69
pardipYadav <213sunilyadav@gmail.com>
2025-05-22 04:58:18 +0530

    loop in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,60 @@
-import { useState } from "react";
 const App = () => {
-  const [radioval, setRadioVal] = useState("male");
-  const [city, setCity] = useState("chandigarh");
-
+  const userS = [
+    {
+      id: 1,
+      name: "pardeep",
+      age: 28,
+      email: "yadavpardeep23@gmail.com",
+    },
+    {
+      id: 2,
+      name: "simran",
+      age: 22,
+      email: "simran@gmail.com",
+    },
+    {
+      id: 3,
+      name: "gunjan",
+      age: 23,
+      email: "gunjan@gmail.com",
+    },
+    {
+      id: 4,
+      name: "preeti",
+      age: 32,
+      email: "preeti@gmail.com",
+    },
+    {
+      id: 5,
+      name: "rahul",
+      age: 31,
+      email: "rahul@gmail.com",
+    },
+  ];
+  console.log("userS", userS);
   return (
     <div>
-      <h1>Radio or dropdown value </h1>
-      <h2>You seleted : {radioval}</h2>
-      <input
-        type="radio"
-        onChange={(event) => {
-          setRadioVal(event.target.value);
-        }}
-        value={"male"}
-        name="genders"
-        checked={radioval === "male"}
-        id="male"
-      />
-      <label htmlFor="male">male</label>
-      <input
-        type="radio"
-        onChange={(event) => {
-          setRadioVal(event.target.value);
-        }}
-        value={"female"}
-        name="genders"
-        checked={radioval === "female"}
-        id="female"
-      />
-      <label htmlFor="female">female</label>
-      {/* <Skills /> */}
-      <br></br>
-      <br></br>
-      <span>
-        You selected the <b>{city}</b> location
-      </span>
-      <h3>Please select location</h3>
-      <select
-        onChange={(event) => {
-          setCity(event.target.value);
-        }}
-        defaultValue={"chandigarh"}
-      >
-        <option value={"delhi"}>delhi</option>
-        <option value={"noida"}>noida</option>
-        <option value={"gurugram"}>gurugram</option>
-        <option value={"chandigarh"}>chandigarh</option>
-      </select>
+      <h1>Loop in JSX </h1>
+      <table border={1}>
+        <thead>
+          <tr>
+            <td>id</td>
+            <td>name</td>
+            <td>age</td>
+            <td>email</td>
+          </tr>
+        </thead>
+        <tbody>
+          {userS.map((item) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.age}</td>
+              <td>{item.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
