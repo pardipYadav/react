55802df56287848e5b95ccc5d2f2e83f59062408
pardipYadav <213sunilyadav@gmail.com>
2025-05-21 05:33:03 +0530

    Controlled Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 const App = () => {
   const [inputval, setInputval] = useState("");
+  const [pass, setPass] = useState("");
+  const [email, setEmail] = useState("");
   return (
     <div>
       <h1>Get input field value</h1>
@@ -13,15 +15,34 @@ const App = () => {
         }}
         placeholder="enter your name"
       ></input>
-      <h1>{inputval}</h1>
-
+      <input
+        type="text"
+        value={pass}
+        onChange={(event) => {
+          setPass(event.target.value);
+        }}
+        placeholder="enter password"
+      ></input>
+      <input
+        type="email"
+        value={email}
+        onChange={(event) => {
+          setEmail(event.target.value);
+        }}
+        placeholder="enter email"
+      ></input>
       <button
         onClick={() => {
           setInputval("");
+          setPass("");
+          setEmail("");
         }}
       >
         clear value
       </button>
+      <h1>{inputval}</h1>
+      <h1>{pass}</h1>
+      <h1>{email}</h1>
     </div>
   );
 };
