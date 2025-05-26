b0426e0a9295e5e7d3bb579c9a55ffc1ac943ce8
pardipYadav <213sunilyadav@gmail.com>
2025-05-27 05:04:18 +0530

    forword ref in react 19 version

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
+import { useRef } from "react";
 import Users from "./Users";
 
 const App = () => {
-  const displayName = (name) => {
-    alert(name);
-  };
-  const onClick = () => {
-    alert("this is function not event");
+  const inputRef = useRef(null);
+  const inputRefAction = () => {
+    inputRef.current.value = "200";
+    inputRef.current.focus();
+    inputRef.current.style.color = "red";
   };
   return (
     <div>
-      <h1>pass function component in parent component</h1>
-      <Users displayName={displayName} name="lakhan" onClick={onClick} />
-      <br></br>
-      <Users displayName={displayName} name="ram" onClick={onClick} />
-      <br></br>
-      <Users displayName={displayName} name="rohan" onClick={onClick} />
-      <br></br>
-      <Users displayName={displayName} name="vinay" onClick={onClick} />
+      <h1>Forword Ref</h1>
+      <Users ref={inputRef} />
+      <button onClick={inputRefAction}>inputRef</button>
     </div>
   );
 };
--- src/Users.jsx
+++ src/Users.jsx
@@ -1,10 +1,28 @@
-const Users = ({ displayName, name, onClick }) => {
+/********* old school style before 19 virsion ***********/
+
+/*import { forwardRef } from "react";
+
+const Users = (props, ref) => {
   return (
     <>
-      <button onClick={() => displayName(name)}>click</button>
-      <button onClick={() => onClick()}>Onclick</button>
+      <div>
+        <input type="text" ref={ref} />
+      </div>
     </>
   );
 };
 
+export default forwardRef(Users);*/
+
+/********* old school style before 19 virsion ***********/
+
+const Users = (props) => {
+  return (
+    <>
+      <div>
+        <input type="text" ref={props.ref} />
+      </div>
+    </>
+  );
+};
 export default Users;
