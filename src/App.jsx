5cb0d8c4deac749cb6d3db1a4abd3acc68688327
pardipYadav <213sunilyadav@gmail.com>
2025-05-28 04:30:48 +0530

    use transition hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
-import { useFormStatus } from "react-dom";
+import { useTransition } from "react";
+import { useState } from "react";
 
 const App = () => {
-  const submitHandler = async () => {
-    await new Promise((res) => setTimeout(res, 2000));
-    console.log("res");
-  };
-  const Inputs = () => {
-    const { pending } = useFormStatus();
-    return (
-      <>
-        <input type="<text>" />
-        <br></br>
-        <input type="password" />
-        <br></br>
-        <button disabled={pending}>{pending ? "submiting" : "submit"}</button>
-      </>
-    );
+  // const [pending, setPending] = useState(false);
+  const [pending, startTransition] = useTransition();
+  const handleButton = async () => {
+    // setPending(true);
+    startTransition(async () => {
+      await new Promise((res) => setTimeout(res, 3000));
+    });
+    // setPending(false);
   };
   return (
     <div>
-      <h1>useFormStatus</h1>
-      <form action={submitHandler}>
-        <Inputs />
-      </form>
+      <h1>useTransition hook</h1>
+      {pending ? (
+        <img
+          style={{ width: "100px" }}
+          src="https://i.gifer.com/origin/34/34338d26023e5515f6cc8969aa027bca_w200.gif"
+          alt=""
+        />
+      ) : null}
+      <button disabled={pending} onClick={handleButton}>
+        clicks
+      </button>
     </div>
   );
 };
