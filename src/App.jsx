d1295a7a65f854bf66ff8b4e2dba6a8abf3eeee4
pardipYadav <213sunilyadav@gmail.com>
2025-05-28 05:52:41 +0530

    understand the drived method

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import { useTransition } from "react";
 import { useState } from "react";
 
 const App = () => {
-  // const [pending, setPending] = useState(false);
-  const [pending, startTransition] = useTransition();
-  const handleButton = async () => {
-    // setPending(true);
-    startTransition(async () => {
-      await new Promise((res) => setTimeout(res, 3000));
-    });
-    // setPending(false);
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState();
+  const handleSubmit = () => {
+    setUsers([...users, user]);
   };
+  const length = users.length;
+  const lUser = users[users.length - 1];
+  const unique = [...new Set(users)].length;
   return (
     <div>
-      <h1>useTransition hook</h1>
-      {pending ? (
-        <img
-          style={{ width: "100px" }}
-          src="https://i.gifer.com/origin/34/34338d26023e5515f6cc8969aa027bca_w200.gif"
-          alt=""
-        />
-      ) : null}
-      <button disabled={pending} onClick={handleButton}>
-        clicks
-      </button>
+      <h1>Drived method</h1>
+      <h3>total users: {length}</h3>
+      <h3>last users: {lUser} </h3>
+      <h3>Unique users: {unique}</h3>
+      <input
+        type="text"
+        onChange={(event) => setUser(event.target.value)}
+        placeholder="Add user"
+      />
+      <button onClick={handleSubmit}>Submit</button>
+      {users.map((item, index) => (
+        <h4 key={index}>{item}</h4>
+      ))}
     </div>
   );
 };
