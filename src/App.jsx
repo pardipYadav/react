d500d4d64f4208e11182b007c2c14581123c6fd6
pardipYadav <213sunilyadav@gmail.com>
2025-05-31 05:10:42 +0530

    useId hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,25 @@
-import { useActionState } from "react";
+import { useId } from "react";
 
 const App = () => {
-  const handleForm = async (prevousepreviousState, FormData) => {
-    let uName = FormData.get("username");
-    let upass = FormData.get("password");
-    await new Promise((res) => setTimeout(res, 2000));
-    if (uName && upass) {
-      return { message: "Data submited ", uName, upass };
-    } else {
-      return { error: " Failed to submit enter proper data", uName, upass };
-    }
-  };
-  const [state, formAction, isPending] = useActionState(handleForm, undefined);
+  const name = useId();
+  const username = useId();
+  const password = useId();
+
   return (
     <div>
-      <h1>UseActionState Hook</h1>
-      <form action={formAction}>
-        <input type="text" name="username" placeholder="enter username" />
+      <h1>useId Hook</h1>
+      <form action="">
+        <label htmlFor={name}>name</label>
+        <input type="text" name="" id={name} />
         <br />
         <br />
-        <input
-          type="password"
-          name="password"
-          id=""
-          placeholder="enter password"
-        />
+        <label htmlFor={username}>username</label>
+        <input type="text" id={username} />
         <br />
         <br />
-        <button disabled={isPending} type="submit">
-          Submit
-        </button>
-        <br />
-        <br />
-        {state?.message && (
-          <span style={{ color: "green" }}>{state?.message}</span>
-        )}
-        {state?.error && <span style={{ color: "red" }}>{state?.error}</span>}
+        <label htmlFor={password}>password</label>
+        <input type="text" id={password} />
       </form>
-      <h4>userName:{state?.uName}</h4>
-      <h4>userPass:{state?.upass}</h4>
     </div>
   );
 };
