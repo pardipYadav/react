4d2b18362d78422c14d1df5c0c45c017b70e8661
pardipYadav <213sunilyadav@gmail.com>
2025-06-02 04:55:38 +0530

    create custom hook with toggle functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
-import { useState } from "react";
-import Collage from "./ContextComponents/Collage";
-import { subjectContext } from "./ContextData";
+import useToggle from "./ContextComponents/Hooks/useToggle";
+
 const App = () => {
-  const [sub, setSub] = useState("");
-  const handleContext = (value) => {
-    setSub(value);
-  };
+  const [value, toggleValue] = useToggle(true);
   return (
-    <div style={{ backgroundColor: "yellow", padding: "10px" }}>
-      <h1>Context Api</h1>
-      <select
-        value={sub}
-        onChange={(event) => handleContext(event.target.value)}
-      >
-        <option value="">Select option</option>
-        <option value="English">English</option>
-        <option value="Science">Science</option>
-        <option value="Geography">Geography</option>
-      </select>
-      <button onClick={() => setSub("")}>Clear subject</button>
-      <subjectContext.Provider value={sub}>
-        <Collage />
-      </subjectContext.Provider>
+    <div>
+      <button onClick={toggleValue}>toggle</button>
+      <button onClick={() => toggleValue(false)}>hide toggle</button>
+      <button onClick={() => toggleValue(true)}>show toggle</button>
+      {value ? <h1>custom hook useToggle</h1> : null}
     </div>
   );
 };
